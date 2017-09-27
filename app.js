e529af80742ec5ebb55df1d61799a8e3d544b298
saidsalti <firist821@gmail.com>
2017-09-27 08:42:15 +0400

    add new items
    i copy my items from another project

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-var http = require('http');
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World\n");
+
+const express = require('express');
+const bodyParser= require('body-parser');
+var hbs = require('hbs');
+
+var router = require('./server/routes/index');
+var app = express();
+app.use(bodyParser.json());
+var port=process.env.PORT||4100;
+
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname + '/public'));
+
+ app.use('/',router.home);
+ app.use('/api',router.users);
+app.use(function(err,req,res,next){
+
+   res.status(422).send({"error":err.message});
 });
-server.listen(80);
-console.log("Server running at http://165.227.161.248:80/");
+
+app.listen(port,()=>{
+  console.log(`http://localhost:${port}`);
+});
---
+++ server/query/user.js
@@ -0,0 +1,21 @@
+var {mongoose}= require('../db/mongoose');
+
+var User = require('../models/user');
+
+// var said = new User({
+//   firstName:'said1',
+//   secondName:'Yaqoob',
+//   lastName:'Alsalti',
+//   email:'said.salti1@moe.om',
+//   gender:1
+// });
+
+// said.save(function(err){
+// console.log(err);
+// });
+
+// User.findById("59c62e3f86149c2b8bc646b5",(err,res)=>{
+// if(err)
+//   return console.log(err);
+// console.log(JSON.stringify(res));
+// });
