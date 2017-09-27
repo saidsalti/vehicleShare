abc5d3fb4c45ce23c62fc6e502c39699176f32a7
saidsalti <firist821@gmail.com>
2017-09-27 13:59:12 +0400

    w

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+var hbs = require('hbs');
 
 var port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
+    res.render("home");
+});
+app.get("/test", (req, res) => {
 
     res.send("Welcom");
 });
 app.listen(port, function() {
-    console.log("open");
+    console.log('http://localhost:3000')
 })
