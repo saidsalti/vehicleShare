a3ec7907c381c1d366233c9ba3c4de5faa70b4ed
saidsalti <firist821@gmail.com>
2017-09-27 23:08:12 +0400

    1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vsharedb');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vsharedb');
+mongoose.connect('mongodb://localhost:27017/vsharedb');
 
 module.exports = { mongoose };
